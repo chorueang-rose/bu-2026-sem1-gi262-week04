726cf195bdc36a479ed7d867a7dbe260a22b9783
chorueang-rose <iamrobinxz@gmail.com>
2026-09-08 11:02:19 +0700

    งานในคาบเรียนให้ไปทำเป็นการบ้านต่อ

--- Assets/Scripts/Workspace/Assignment/Lecture.cs
+++ Assets/Scripts/Workspace/Assignment/Lecture.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System.Collections;
 
 namespace Assignment
 {
@@ -8,9 +9,9 @@ namespace Assignment
         public void Start()
         {
             // LCT01_SyntaxList();
-            // LCT02_SyntaxLinkedList();
+            //LCT02_SyntaxLinkedList();
             // LCT03_SyntaxHashTable();
-            // LCT04_SyntaxDictionary();
+            //LCT04_SyntaxDictionary();
         }
 
         #region Lecture
@@ -22,19 +23,101 @@ namespace Assignment
 
         public void LCT02_SyntaxLinkedList()
         {
-            throw new System.NotImplementedException();
+            //string[] playerName = new string[20];
+            LinkedList<string> linkedlist = new LinkedList<string>();
+            
+            //[Node 1]
+            linkedlist.AddLast("Node 1");
+
+            //[Node 1]<-[Node 2]
+            linkedlist.AddLast("Node 2");
+
+            //[Node 0]<-[Node 1]<-[Node 2]
+            linkedlist.AddFirst("Node 0");
+
+            LinkedListNode<string> firstNode = linkedlist.First;
+            Debug.Log("first" + firstNode.Value);
+
+            LinkedListNode<string> lastNode = linkedlist.First;
+            Debug.Log("last" + lastNode.Value);
+
+            Debug.Log("firstNode.Next: " + firstNode.Next.Value);
+            Debug.Log("firstNode.Next.Next: " + firstNode.Next.Next.Value);
+
+            Debug.Log("lastNode.Previous: " + lastNode.Previous.Value);
+            Debug.Log("lastNode.Previous.Previous: " + lastNode.Previous.Previous.Value);
+
+            if(firstNode.Previous == null) Debug.Log("firstNode.Previous == null");
+            if (lastNode.Next == null) Debug.Log("lastNode.Next == null");
+
+            //[Node 0]<-[Node 0.5]<-[Node 1]<-[Node 2]
+            linkedlist.AddAfter(firstNode, "Node 0.5");
+
+            //[Node 0]<-[Node 0.5]<-[Node 1]<-[Node 1.5]<-[Node 2]
+            linkedlist.AddBefore(lastNode, "Node 0.5");
+
+            LinkedListNode<string> node1 = linkedlist.Find("Node 1");
+
+            linkedlist.Remove("Node 1");
+            linkedlist.Remove(node1);
+            linkedlist.RemoveLast();
+            linkedlist.RemoveFirst();
+
+            linkedlist.Clear();
+            
         }
 
         public void LCT03_SyntaxHashTable()
         {
-            throw new System.NotImplementedException();
+            Hashtable table = new Hashtable();
+            table.Add("Potion", 1);
+            table.Add(true, "");
+            table.Add(0, 0);
+            table[true] = 1;
         }
 
         public void LCT04_SyntaxDictionary()
         {
-            throw new System.NotImplementedException();
-        }
+          Dictionary<string, int> inv = new Dictionary<string, int>();
+            var inv2 = new Dictionary<string, int>();
 
+            inv.Add("Potion", 1);
+
+            //"Poion" : 1
+            //"Apple" : 10
+            inv.Add("Apple", 10);
+
+            inv["Banana"] = 5;
+
+            inv["Potion"] = 10;
+
+            var pickupItem = "Sowrd";
+            inv[pickupItem] = 1;
+
+            //foreach(KeyValuePair<string, int> pair in inv)
+            foreach (var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
+            var appleExists = inv.ContainsKey("Key");
+            Debug.Log(appleExists);
+
+            var keyExists = inv.ContainsKey("Key");
+            Debug.Log(keyExists);
+            
+            inv.Remove("Apple");
+
+            foreach (var pair in inv)
+            {
+                string key = pair.Key;
+                int value = pair.Value;
+                Debug.Log($"Key: {key} value: {value}");
+            }
+
+        }
         #endregion
     }
 }
